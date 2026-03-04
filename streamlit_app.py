c7e60b2e0934558fe11a05d85c57b8e15845ba8e
JesusASantillanMinila <alosantillan@gmail.com>
2026-03-04 14:38:56 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,32 +1,16 @@
-import streamlit as st
-from streamlit_gsheets import GSheetsConnection
-import pandas as pd
 
-st.title("🏀 March Madness Bracket Predictor")
+# import streamlit as st
+# import pandas as pd
+# from google.oauth2 import service_account
+# from googleapiclient.discovery import build
 
-# --- Read CSVs from Google Drive ---
-def load_drive_csv(file_id):
-    # Construct the direct download URL for Google Drive
-    url = f'https://drive.google.com/uc?export=download&id={file_id}'
-    return pd.read_csv(url)
 
-# --- Read Documents ---
-try:
-    conn = st.connection("gsheets", type=GSheetsConnection)
-    df_gsheet = conn.read(
-        spreadsheet=st.secrets["ids"]["KAGGLE_SUBMISSION_ID"],
-        worksheet="data"
-    )
 
-    df_m_teams = load_drive_csv(st.secrets["ids"]["M_TEAM_NAMES_ID"])
-    df_w_teams = load_drive_csv(st.secrets["ids"]["W_TEAM_NAMES_ID"])
-    
-    
-except Exception as e:
-    st.error(f"Error loading document: {e}")
+# st.title("🏀 March Madness Bracket Predictor")
 
-st.dataframe(df_gsheet)
 
-st.dataframe(df_m_teams)
+# st.dataframe(df_gsheet)
 
-st.dataframe(df_w_teams)
+# st.dataframe(df_m_teams)
+
+# st.dataframe(df_w_teams)
