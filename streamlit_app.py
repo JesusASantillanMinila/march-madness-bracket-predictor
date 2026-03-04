591b1f1f4152887f34ef8f9027a251931adb4d00
JesusASantillanMinila <alosantillan@gmail.com>
2026-03-04 14:30:29 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,32 @@
 import streamlit as st
+from streamlit_gsheets import GSheetsConnection
+import pandas as pd
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("🏀 March Madness Bracket Predictor")
+
+# --- Read CSVs from Google Drive ---
+def load_drive_csv(file_id):
+    # Construct the direct download URL for Google Drive
+    url = f'https://drive.google.com/uc?export=download&id={file_id}'
+    return pd.read_csv(url)
+
+# --- Read Documents ---
+try:
+    conn = st.connection("gsheets", type=GSheetsConnection)
+    df_gsheet = conn.read(
+        spreadsheet=st.secrets["ids"]["KAGGLE_SUBMISSION_ID"],
+        worksheet="data"
+    )
+
+    df_m_teams = load_drive_csv(st.secrets["ids"]["M_TEAM_NAMES_ID"])
+    df_w_teams = load_drive_csv(st.secrets["ids"]["W_TEAM_NAMES_ID"])
+    
+    
+except Exception as e:
+    st.error(f"Error loading document: {e}")
+
+st.dataframe(df_gsheet)
+
+st.dataframe(df_m_teams)
+
+st.dataframe(df_w_teams)
